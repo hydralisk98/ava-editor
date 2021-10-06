94e965b726e250b506deb9b31d07cc4015f33296
hl98 <olyvier.bouchard@gmail.com>
2021-10-06 10:03:26 -0400

    Preparing Ava System integration

---
+++ AvaSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ava
+{
+    class AvaSystem
+    {
+        private string binary;
+        private string plainText;
+        private string avaString;
+
+        // define Ava system
+        char[] avaSymbolSet = new char[12] { 'A', 'B', 'C', 'D', 'E', 'F', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        /*
+        public string Encode(string input)
+        {
+
+        }
+
+        public string Decode(string input)
+        {
+
+        }
+        */
+    }
+}
